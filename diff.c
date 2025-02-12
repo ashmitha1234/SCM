0dd57d4877f12c124aafd2ca9163476ad7ba0b47
ashmitha1234 <ashmitha.s@s.amity.edu>
2025-02-12 06:56:42 +0530

    This is a C code to find the difference between two numbers entered by the user

---
+++ diff.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+	int num1, num2, diff;
+	printf("Enter the values of num1 and num2 : ");
+	scanf("%d %d", &num1, &num2);
+	diff = num1 -  num2;
+	printf("The difference between num1 and num2 is : %d", diff);
+	return 0;
+}
+
