4a8daa8d41739cc809cf4f754c381c46a01cfac6
ashmitha1234 <ashmitha.s@s.amity.edu>
2025-02-18 19:22:56 +0530

    C program to find sum of two numbers

--- sum.c
+++ sum.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main(){
-	int a=3, b=5;
-	int sum;
-	sum = a + b;
-	printf("The sum = %d", sum);
+	int num1, num2;
+	printf("Enter num1 and num2");
+	sacnf("%d %d",&num1, &num2);
+	printf("The sum = %d", num1+num2);
 }
