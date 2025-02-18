f0023bf2d749cd76ad0c35be6476300d3c59296b
ashmitha1234 <ashmitha.s@s.amity.edu>
2025-02-18 12:17:30 +0530

    Python program to swap two numbers

---
+++ swapnum.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter first number")
+num2=int(input("Enter second number")
+def swap(a,b):
+    a=a+b
+    b=a-b
+    a=a-b
+    print("a = ", a)
+    print("b =" , b)
+swap(a,b)
+
