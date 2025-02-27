84535a84459f2a8f2e7749239b1269aaf632d66d
ashmitha1234 <ashmitha.s@s.amity.edu>
2025-02-27 21:01:49 +0530

    Changed the variable names

--- armstrong.c
+++ armstrong.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-void main() {
-    int num, originalNum, remainder, result = 0;
-    printf("Enter a three-digit integer: ");
-    scanf("%d", &num);
-    originalNum = num;
-
-    while (originalNum != 0) {
-       remainder = originalNum % 10;
-       result += remainder * remainder * remainder;
-       originalNum /= 10;
+void main(){
+    int num, copy, sum=0;
+    printf("Enter a number : ");
+    scanf("%d", &num); //153
+    copy = num;
+    while(num!=0){
+    int d = num % 10; //3
+    sum += d * d * d; //9
+    num = num / 10; //15
     }
-
-    if (result == num)
-        printf("%d is an Armstrong number.", num);
+    if (sum == copy)
+        printf("%d is an Armstrong number.", copy);
     else
-        printf("%d is not an Armstrong number.", num);
+        printf("%d is not an Armstrong number.", copy);
 }
