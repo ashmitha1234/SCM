1a867381220d234db4f999632f7c8632e6a5b6e7
ashmitha1234 <ashmitha.s@s.amity.edu>
2025-03-04 11:06:10 +0530

    The else statement here checks if the number is odd

--- evenodd.c
+++ evenodd.c
@@ -4,6 +4,8 @@ void main(){
 	printf("Enter a number : ");
 	scsnf("%d", &num);
 	if(num%2==0)
-		printf("The given number is even")
+		printf("The given number is an even number");
+	else
+		printf("The given number is a odd number");
 }
 
