54dc3717683e885ffcc779f49785b63fbb141a06
ashmitha1234 <ashmitha.s@s.amity.edu>
2025-03-25 11:20:56 +0530

    Added case 1: Selected for CSE

---
+++ exp1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int choice;
+	printf("Enter : 1 - for Btech(CSE)\n 2 - for BTech(AI&ML)\N 3 - for BTech(ECE)\N 4 - for BTech(Mechanical)\n");
+        scanf("%d ", &choice);
+	switch(choice)
+	{
+		case 1: 
+			printf("You selected Computer Science (CS).\n");
+		}
+        return 0;
+}
+
