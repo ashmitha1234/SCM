f55d4b35e39bd0c8ddcbe70e88ea12b7a37d1719
ashmitha1234 <ashmitha.s@s.amity.edu>
2025-04-22 11:08:36 +0530

    C++ program to print different data types

---
+++ p2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i=55; float j=45.68; double k=21455.98765; bool x=true;
+	cout<<"i="<<i<<"\tj="<<j<<"\tk="<<k<<"\tx="<<x;
+	return 0;
+}
