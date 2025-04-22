38cf1783f21b6bb586a294fb0329829e1c2632ce
ashmitha1234 <ashmitha.s@s.amity.edu>
2025-04-22 11:16:15 +0530

    C++ program to perform arithematic operations

---
+++ p3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i; int j; int sum=0;
+	cout<<"Enter value of i : ";
+	cin>>i;
+	cout<<"Enter value of j : ";
+        cin>>j;
+        cout<<"The sum is : "<<i+j<<"\nThe difference is : "<<i-j<<"\nThe product is : "<<i*j<<"the quotient is : " <<j/i;
+	return 0;
+}
